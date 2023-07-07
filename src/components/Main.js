2b1a33a54c92e5d12025012c9ae801a3107714f0
Elena <elenadiaz32505@gmail.com>
2023-07-07 13:45:29 -0500

    updated images

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,19 @@ import React from "react";
 export default function Main(props) {
 	return (
 		<main className={props.darkMode ? "dark" : ""}>
-			<h1 className="main--title">Fun facts about React</h1>
+			<h1 className="main--title">Fun facts about Elena</h1>
 			<ul className="main--facts">
-				<li>Was first released in 2013</li>
-				<li>Was originally created by Jordan Walke</li>
-				<li>Has well over 100K stars on GitHub</li>
-				<li>Is maintained by Facebook</li>
-				<li>Powers thousands of enterprise apps, including mobile apps</li>
+				<li>
+					Multilingual: Fluent in English, Spanish, currently learning Japanese
+					and Korean.
+				</li>
+				<li>Agile Developer: Quick learner, adaptable to new technologies.</li>
+				<li>Design Skills: Google UX certified, skilled in Figma.</li>
+				<li>Tech Stack: React, Javascript, HTML, CSS</li>
+				<li>
+					Diverse Interests: Inline skating, photography, digital art, Korean &
+					Japanese culture and media.
+				</li>
 			</ul>
 		</main>
 	);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "../images/react-icon-small.png";
+import logo from "../images/Self-Portrait.png";
 
 export default function Navbar(props) {
 	return (
 		<nav className={props.darkMode ? "dark" : ""}>
 			<img className="nav--logo_icon" src={logo} alt="React logo" />
-			<h3 className="nav--logo_text">ReactFacts</h3>
+			<h3 className="nav--logo_text">Elena_Facts</h3>
 
 			<div className="toggler">
 				<p className="toggler--light">Light</p>
